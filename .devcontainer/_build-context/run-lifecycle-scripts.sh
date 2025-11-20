65c2ab6e9042b0662af3daf005236b2f86252029
Yanhao-MBP <Yanhao@1-H.CC>
2025-11-20 13:40:53 +0800

    refactor: 更新生命周期脚本，优化输出信息和执行权限设置

--- .devcontainer/_build-context/run-lifecycle-scripts.sh
+++ .devcontainer/_build-context/run-lifecycle-scripts.sh
@@ -12,9 +12,9 @@ USER_SCRIPTS_DIR=$(find /usr/local/etc/lifecycle-scripts.d -maxdepth 1 -type d -
 
 if [ -n "$USER_SCRIPTS_DIR" ] && [ -d "$USER_SCRIPTS_DIR" ]; then
     if [ -n "$(ls -A "$USER_SCRIPTS_DIR")" ]; then
-        echo "--- 赋予用户自定义脚本可执行权限 ---"
+        # echo "--- 赋予用户自定义脚本可执行权限 ---"
         chmod +x "$USER_SCRIPTS_DIR"/*
-        echo "▶️ --- 开始执行 ${LIFECYCLE_EVENT} 用户自定义脚本: $USER_SCRIPTS_DIR ---"
+        echo "🚀 --- 开始执行 ${LIFECYCLE_EVENT} 用户自定义脚本: $USER_SCRIPTS_DIR ---"
         run-parts --verbose "$USER_SCRIPTS_DIR"
         echo "✔️ --- ${LIFECYCLE_EVENT} 用户自定义脚本执行完毕 ---"
     else
